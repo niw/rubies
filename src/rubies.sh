dbaa936741f977262438107e899329b8b124c31e
Yoshimasa Niwa <niw@niw.at>
2011-06-19 22:27:31 -0700

    Rename internal functions.

--- src/rubies.sh
+++ src/rubies.sh
@@ -1,15 +1,15 @@
-exec_ruby() {
+_rubies_exec_ruby() {
   local path_to_ruby=$1; shift
   /usr/bin/env -i PATH="$PATH" HOME="$HOME" $path_to_ruby - $*
 }
 
-eval_rb_result() {
+_rubies_eval_rb_result() {
   # FIXME select ruby if it is in .rubies.
-  eval "$(exec_ruby /usr/bin/ruby $*)"
+  eval "$(_rubies_exec_ruby /usr/bin/ruby $*)"
 }
 
 rubies() {
-  eval_rb_result $* << 'END_OF_RB'
+  _rubies_eval_rb_result $* << 'END_OF_RB'
     base_path = File.expand_path(".rubies", ENV["HOME"])
     $LOAD_PATH.unshift File.join(base_path, "src")
     require "rubies"
