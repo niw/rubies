62e96b0290c83a0c379747132be6077e01437a1f
Yoshimasa Niwa <niw@niw.at>
2011-08-27 01:10:44 +0900

    Add ruby-1.9.2-p290.

---
+++ src/ruby-1.9.2-p290.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Select normal GCC.
+# Once we install Xcode 4.2, it will replace /usr/bin/gcc to llvm-gcc
+# which causes some unexpected error for now.
+if [ -f /usr/bin/gcc-4.2 ]; then
+	export CC=/usr/bin/gcc-4.2
+fi
+if [ -f /usr/bin/g++-4.2 ]; then
+	export CXX=/usr/bin/g++-4.2
+fi
+
+# Use OS X default libraries except readline.
+# I've made readline5 formura for Homebrew, because Homebrew doesn't have it.
+../configure \
+	--prefix "$HOME/.rubies/ruby-1.9.2-p290" \
+	--disable-option-checking \
+	--with-zlib-dir=/usr/lib \
+	--with-ncurses-dir=/usr/lib \
+	--with-readline-dir=`brew --prefix readline5` \
+	--with-libyaml-dir=`brew --prefix libyaml` \
+	--with-iconv-dir=/usr/lib \
+	--with-openssl-dir=/usr/lib \
+	--enable-shared \
+	--disable-install-doc \
+	--with-arch=x86_64
